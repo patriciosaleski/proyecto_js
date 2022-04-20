6304774740a52c4576a8603ddc16dd70fc71afc1
patriciosaleski <patriciosaleski@hotmail.com>
2022-04-20 15:03:18 -0300

    Se crean los archivos para el desafio 2

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-// Este código nombrará todos los números naturales mayores iguales que 3, múltiplos de 3 y 5 hasta el número dado, luego dará como resultado la suma de todos esos números múltiplos.
+const $botonCalcular = document.querySelector('#calcular')
+const $botonReset = document.querySelector('#reset')
 
-let numero
-let suma
+$botonCalcular.onclick = function(){
+    const absoluteZero = -459.67
+    const valor = Number(document.querySelector('#grados-F').value)
 
-suma = 0
-numero = parseInt(prompt('Ingrese un número mayor o igual a 3'))
+    if (isNaN(valor)) {
+        alert('Ingrese un valor válido.')
+    } else if (valor < absoluteZero) {
+        alert('Ese valor se encuentra por debajo del cero absoluto (-459.67 °F), intente con un mayor valor.')
+        return;
+    } else {
+        resultado = ((valor - 32) * 5/9).toFixed(2)
 
-for (let i = 1; i <= numero; i++) {
-    if ((i % 5 == 0 || i % 3 == 0) && i >= 3) {
-        document.writeln(i)
-        suma += i
+        if (resultado > 40) {
+            document.querySelector('#resultado').value = `${resultado}°C`
+            alert('Ponete protector solar!')
+        } else {
+            document.querySelector('#resultado').value = `${resultado}°C`
+        }
+        
     }
 }
 
-if (isNaN(numero) || numero < 3) { 
-    document.write('Ingrese un número válido')
-} else {
-    if (suma % 5 == 0 && suma %3 == 0 && numero >= 3){
-        document.write(`Todos los números múltiplos de 3 y 5 en el rango del 0 al ${numero} dieron ${suma}, el cual también es múltiplo de 3 y 5.`)
-
-    } else if (suma % 5 == 0 && numero >= 3) {
-        document.write(`Todos los números múltiplos de 3 y 5 en el rango del 0 al ${numero} dieron ${suma}, el cual también es múltiplo de 5.`)
-
-    } else if (suma % 3 == 0 && numero >= 3) {
-        document.write(`Todos los números múltiplos de 3 y 5 en el rango del 0 al ${numero} dieron ${suma}, el cual también es múltiplo de 3.`)
-
-    } else if (numero >= 3) {
-        document.write(`Todos los números múltiplos de 3 y 5 en el rango del 0 al ${numero} dieron ${suma}.`)
-    }
+$botonReset.onclick = function(){
+    document.querySelector('#grados-F').value = ""
+    document.querySelector('#resultado').value = ""
 }
